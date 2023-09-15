106b7555f6581bcd5094154eb15f64609e7e7449
Dedalino <kelsonpapelo18@gmail.com>
2023-09-15 20:23:56 +0100

    serve request with sendFile

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let app = express();
+const path = require("path")
 
 app.get("/", function(req, res){
-    res.send("Hello Express")
+    res.sendFile(path.join(__dirname, '/views/index.html'))
   })
 
   console.log("Hello World")
