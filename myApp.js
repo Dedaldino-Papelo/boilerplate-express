19bb56345720f5d121b1aa15f5ab4c476da603d1
Dedalino <kelsonpapelo18@gmail.com>
2023-09-15 23:07:44 +0100

    serve JSON on a specific route

--- myApp.js
+++ myApp.js
@@ -6,7 +6,13 @@ app.use('/public', express.static(path.join(__dirname + '/public')))
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, '/views/index.html'))
-  })
+})
+
+app.get("/json", function(req, res){
+    res.json({
+        message: "Hello json"
+    })
+})
 
   console.log("Hello World")
 
