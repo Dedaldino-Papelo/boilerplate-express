7b1d9643c5e5919d161d2a5a6e192e2cb7973410
Dedalino <kelsonpapelo18@gmail.com>
2023-09-15 22:50:22 +0100

    serve static files

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ let express = require('express');
 let app = express();
 const path = require("path")
 
+app.use(path.join(__dirname + '/public'))
+
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, '/views/index.html'))
   })
