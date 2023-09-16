b1285742d5b086c900b0151aa6d8aab01e250d54
Dedalino <kelsonpapelo18@gmail.com>
2023-09-16 23:14:52 +0100

    fix

--- myApp.js
+++ myApp.js
@@ -3,20 +3,22 @@ let express = require('express');
 let app = express();
 const path = require("path")
 
+process.env.MESSAGE_STYLE == "uppercase";
+
 app.use('/public', express.static(path.join(__dirname + '/public')))
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
-app.get('/json', (req, res) => {
-    const MESSAGE_STYLE = process.env.MESSAGE_STYLE
+var message = {"message": "Hello json"};
 
-    if (MESSAGE_STYLE === 'uppercase') {
-        res.json({ message: 'HELLO JSON' })
-      } else {
-        res.json({ message: 'Hello json' });
-      } 
+app.get('/json', (req, res) => {
+    if(process.env.MESSAGE_STYLE == uppercase){
+        response.json({"message" : "HELLO JSON"})
+     }else{
+       response.json(message)
+     }
 })
 
 
