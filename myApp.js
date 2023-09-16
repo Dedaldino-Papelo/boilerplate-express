855f35da99ccd782e59ca2194f98b8b4a30613ab
Dedalino <kelsonpapelo18@gmail.com>
2023-09-16 17:33:23 +0100

    fix

--- myApp.js
+++ myApp.js
@@ -11,12 +11,14 @@ app.get("/", function(req, res){
 
 app.get("/json", function(req, res){
     const mySecret = process.env['MESSAGE_STYLE']
-    
+
     if (mySecret === "uppercase"){
-     res.json({message: "Hello json".toUpperCase()});
+     res.json({"message": "Hello json".toUpperCase()});
   }
   else
-    res.json({message: "Hello json"} );
+    res.json({
+        "message": "Hello json"
+    } );
 
 })
 
