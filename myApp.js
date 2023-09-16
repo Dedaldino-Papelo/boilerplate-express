a72fa8f9a756a95f04b58d35f2adf915168ab572
Dedalino <kelsonpapelo18@gmail.com>
2023-09-16 20:31:24 +0100

    fix: logger middleware

--- myApp.js
+++ myApp.js
@@ -6,10 +6,10 @@ require('dotenv').config()
 //middlewares
 app.use('/public', express.static(path.join(__dirname + '/public')))
 
-app.use(function middleware(req, res, next){
-    console.log(req.method + ' ' + req.path + ' - ' + req.ip)
-    next()
-})
+app.use(function middleware(req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+  });
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, '/views/index.html'))
