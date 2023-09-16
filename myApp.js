c0fae7905b0f7be8a935961c4915f59150a72ab4
Dedalino <kelsonpapelo18@gmail.com>
2023-09-16 20:06:09 +0100

    fix: Hello Json exercise

--- myApp.js
+++ myApp.js
@@ -5,23 +5,19 @@ require('dotenv').config()
 
 //middlewares
 app.use('/public', express.static(path.join(__dirname + '/public')))
-app.use(function(req, res, next){
-    console.log(`${req.method} ${req.path} - ${req.ip}`)
-    next()
-})
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
-app.get("/json", function(req, res){
-    const messageStyle = process.env.MESSAGE_STYLE || "uppercase";
+let dataJson = {"message": "Hello json"};
 
-  if (messageStyle === "uppercase") {
-    res.json({ message: "HELLO JSON" });
-  } else {
-    res.json({ message: "Hello json" });
-  }
+app.get("/json", function(req, res){
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        dataJson.message = dataJson.message.toUpperCase();
+      };
+      
+      res.json(dataJson);
 })
 
   console.log("Hello World")
