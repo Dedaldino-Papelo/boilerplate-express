5382a9f7106d9a18ba5afa8c3f236f82fc170c8c
Dedalino <kelsonpapelo18@gmail.com>
2023-09-16 22:13:24 +0100

    fix

--- myApp.js
+++ myApp.js
@@ -9,12 +9,12 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
-let dataJson = {"message": "Hello json"};
+let dataJson = {message: "Hello json"};
 
 app.get('/json', function(req, res){
     const MESSAGE_STYLE = process.env.MESSAGE_STYLE
-    
-    if (MESSAGE_STYLE === 'uppercase') {
+
+    if (MESSAGE_STYLE == 'uppercase') {
         dataJson.message = dataJson.message.toUpperCase();
       };
       
