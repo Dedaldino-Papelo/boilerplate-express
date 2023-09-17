146153b37b5b40d0f8ad599576517ed5c01ce7cb
Dedalino <kelsonpapelo18@gmail.com>
2023-09-17 13:22:33 +0100

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let express = require('express');
 let app = express();
 const path = require("path")
 
-app.use('/public', express.static(path.join(__dirname + '/public')))
+app.use('/public', express.static(__dirname + '/public'))
 
 app.use((req, res, next) => {
   console.log(req.method + " " + req.path + " - " + req.ip);
@@ -11,18 +11,27 @@ app.use((req, res, next) => {
 });
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.sendFile(__dirname + '/views/index.html')
 })
 
+/* app.get('/json', (req, res) => {
+  res.json({ "message": 'Hello json' })
+}) */
+
 app.get('/json', (req, res) => {
-    const mySecret = process.env['MESSAGE_STYLE'];
-    if( mySecret === "uppercase" ){
+    if(process.env['MESSAGE_STYLE'] == "uppercase" ){
       res.json({"message" : "Hello json".toUpperCase() });
     } else {
       res.json({"message" : "Hello json" });
     }
 })
 
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString()
+  next()
+}, (req, res) => {
+  res.json({ time: req.time })
+})
 module.exports = app
 
 
