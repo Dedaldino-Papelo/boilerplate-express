965eb7cc083d126bceb4185f9be926e0cffd756f
Dedalino <kelsonpapelo18@gmail.com>
2023-09-17 13:48:27 +0100

    Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -14,9 +14,6 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + '/views/index.html')
 })
 
-/* app.get('/json', (req, res) => {
-  res.json({ "message": 'Hello json' })
-}) */
 
 app.get('/json', (req, res) => {
     if(process.env['MESSAGE_STYLE'] == "uppercase" ){
@@ -26,12 +23,26 @@ app.get('/json', (req, res) => {
     }
 })
 
+//Chain Middleware to Create a Time Server
 app.get('/now', (req, res, next) => {
   req.time = new Date().toString()
   next()
 }, (req, res) => {
   res.json({ time: req.time })
 })
+
+//Get Route Parameter Input from the Client
+app.get('/:word/echo', (req, res) => {
+  res.json({ echo: req.params.word })
+})
+
+//Get Query Parameter Input from the Client
+app.route('/name')
+  .get((req, res) => {
+    const { first, last } = req.query
+    res.json({ name: first + " " + last })
+  })
+
 module.exports = app
 
 
